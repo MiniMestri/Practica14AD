a7753feb9e56949bf17a943353cb76af6135bee9
MiniMestri <fonsild97@hotmail.com>
2024-02-05 22:36:22 +0100

    creacion de fron

--- back.js
+++ back.js
@@ -1,5 +1,21 @@
 var servidor = require('http');
 var archivo = require('fs');
+var mongoose = require('mongoose')
+
+const conexion='mongodb://127.0.0.1/bbdd'
+
+const formularioSchema = new mongoose.Schema({
+    nombre:String,
+    puntuacion:String,
+    fecha:String
+})
+
+const Clasificacion = mongoose.model("Clasificacion",formularioSchema)
+
+mongoose.connect(conexion,{useNewUrlParser:true,useUnifiedTopology:true}).then(function(){
+    console.log("conectado a mongo")
+    
+})
 
 servidor.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
