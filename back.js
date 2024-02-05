bc5b44030befddf67bb8963ffa3749e63cb3f762
MiniMestri <fonsild97@hotmail.com>
2024-02-05 22:46:25 +0100

    Conexiones a BBDD

--- back.js
+++ back.js
@@ -1,8 +1,9 @@
 var servidor = require('http');
 var archivo = require('fs');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var mysql = require('mysql');
 
-const conexion='mongodb://127.0.0.1/bbdd'
+const conexionmongoose='mongodb://127.0.0.1/bbdd'
 
 const formularioSchema = new mongoose.Schema({
     nombre:String,
@@ -12,10 +13,22 @@ const formularioSchema = new mongoose.Schema({
 
 const Clasificacion = mongoose.model("Clasificacion",formularioSchema)
 
-mongoose.connect(conexion,{useNewUrlParser:true,useUnifiedTopology:true}).then(function(){
+mongoose.connect(conexionmongoose,{useNewUrlParser:true,useUnifiedTopology:true}).then(function(){
     console.log("conectado a mongo")
     
 })
+
+var conexion = mysql.createConnection({
+                host:"localhost",
+                user:"node",
+                password:"1234",
+                database:"nodejs"
+            });
+
+conexion.connect(function(err){
+                if(err) throw err;
+                console.log("conectado")
+ }) 
 
 servidor.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
