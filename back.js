f01988267092ac4cf2450665a176a4999eaf39d9
MiniMestri <fonsild97@hotmail.com>
2024-02-07 19:29:26 +0100

    guardar puntuacion bbdd

--- back.js
+++ back.js
@@ -1,53 +1,80 @@
 var servidor = require('http');
 var archivo = require('fs');
-var mongoose = require('mongoose');
 var mysql = require('mysql');
-
-const conexionmongoose='mongodb://127.0.0.1/bbdd'
-
-const formularioSchema = new mongoose.Schema({
-    nombre:String,
-    puntuacion:String,
-    fecha:String
-})
-
-const Clasificacion = mongoose.model("Clasificacion",formularioSchema)
-
-mongoose.connect(conexionmongoose,{useNewUrlParser:true,useUnifiedTopology:true}).then(function(){
-    console.log("conectado a mongo")
-    
-})
+var url = require('url');
 
 var conexion = mysql.createConnection({
-                host:"localhost",
-                user:"node",
-                password:"1234",
-                database:"nodejs"
-            });
+    host: "localhost",
+    user: "node",
+    password: "1234",
+    database: "nodejs"
+});
 
-conexion.connect(function(err){
-                if(err) throw err;
-                console.log("conectado")
- }) 
+conexion.connect(function (err) {
+    if (err) throw err;
+    console.log("conectado")
+})
 
-servidor.createServer(function(req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-    
-    switch(req.url){
-        case "/":
-            archivo.readFile('front.html', 'utf8', function(err, data) {
-            res.write(data);
-            
-            });
+servidor.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const { pathname } = url.parse(req.url, true);
+
+    switch (req.method) {
+        case "GET":
+            if (pathname === "/") {
+                archivo.readFile('front.html', 'utf8', function (err, data) {
+                    res.write(data);
+                    res.end();
+                });
+            } else if (pathname === "/clasificacion") {
+                archivo.readFile('clasificacion.html', 'utf8', function (err, data) {
+                    res.write(data);
+                    res.end();
+                });
+            } else {
+                res.end("ERROR");
+            }
             break;
-        case "/clasificacion":
-           archivo.readFile('clasificacion.html', 'utf8', function(err, data) {
-            res.write(data);
-           });
-               break;
-        default:  
-        res.end("ERROR")
+
+        case "POST":
+            if (pathname === "/guardar-puntuacion") {
+                let data = '';
+
+                req.on('data', chunk => {
+                    data += chunk;
+                });
+
+                req.on('end', () => {
+                    try {
+                        const { nombre, puntuacion } = JSON.parse(data);
+
+                        const sql = 'INSERT INTO clasificacion (nombre, puntuacion) VALUES (?, ?)';
+                        const values = [nombre, puntuacion];
+
+                        conexion.query(sql, values, (err, result) => {
+                            if (err) {
+                                console.error('Error al insertar en MySQL:', err);
+                                res.statusCode = 500;
+                                res.end(JSON.stringify({ error: 'Error interno del servidor' }));
+                            } else {
+                                console.log('Puntuación guardada en MySQL:', result);
+                                res.end(JSON.stringify({ message: 'Puntuación guardada exitosamente' }));
+                            }
+                        });
+                    } catch (error) {
+                        console.error('Error al analizar datos JSON:', error);
+                        res.statusCode = 400;
+                        res.end(JSON.stringify({ error: 'Formato JSON no válido' }));
+                    }
+                });
+            } else {
+                res.end("ERROR");
+            }
+            break;
+
+        default:
+            res.end("ERROR");
     }
-      
- 
-}).listen(8080)
+
+}).listen(8080);
